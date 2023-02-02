2b28e3f95451ed92b087c6b12b04fb2f4bc73e83
danhtung <tung.bd200568@sis.hust.edu.vn>
2023-02-03 03:13:18 +0700

    add historicalSite model

--- src/models/historicalSite/HistoricalSite.java
+++ src/models/historicalSite/HistoricalSite.java
@@ -1,9 +1,64 @@
 package models.historicalSite;
 
+import java.util.Date;
+
 public class HistoricalSite {
 
-	public HistoricalSite() {
-		// TODO Auto-generated constructor stub
-	}
+    private String name;
+    private Date recognitionDate;
+    private String place;
+    private String summary;
+    private String detail;
+
+    public HistoricalSite() {
+    }
+
+    public HistoricalSite(String name, Date recognitionDate, String place, String summary, String detail) {
+        this.name = name;
+        this.recognitionDate = recognitionDate;
+        this.place = place;
+        this.summary = summary;
+        this.detail = detail;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public void setRecognitionDate(Date recognitionDate) {
+    	this.recognitionDate = (Date) recognitionDate.clone();
+    }
+    
+    public void setPlace(String place) {
+        this.place = place;
+    }
+    
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+    
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    public Date getRecognitionDate() {
+        return (Date) this.recognitionDate.clone();
+    }
+    
+    public String getPlace() {
+        return this.place;
+    }
+    
+    public String getSummary() {
+        return this.summary;
+    }
+    
+    public String getDetail() {
+        return this.detail;
+    }
 
 }
