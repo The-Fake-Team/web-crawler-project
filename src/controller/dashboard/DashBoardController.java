a9fd1535876d98b81d29f29a1dd30e58c4825dc5
danhtung <tung.bd200568@sis.hust.edu.vn>
2023-02-05 12:13:04 +0700

    update controller

--- src/controller/dashboard/DashBoardController.java
+++ src/controller/dashboard/DashBoardController.java
@@ -1,24 +1,52 @@
-package screen;
+package controller.dashboard;
 
-import javafx.event.ActionEvent;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
+import models.festival.Festival;
+import models.historicEvent.HistoricEvent;
+import models.historicalFigure.HistoricalFigure;
+import models.historicalPeriod.HistoricalPeriod;
+import models.historicalSite.HistoricalSite;
+import models.king.King;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-public class DashBoard implements Initializable {
+import controller.festival.FestivalsController;
+import controller.historicEvent.HistoricEventsController;
+import controller.historicalFigure.HistoricalFigureController;
+import controller.historicalPeriod.HistoricalPeriodController;
+import controller.historicalPeriod.HistoricalPeriodsController;
+import controller.historicalSite.HistoricalSitesController;
+import controller.king.KingController;
+import data.FestivalData;
+import data.HistoricEventData;
+import data.HistoricalFigureData;
+import data.HistoricalPeriodData;
+import data.HistoricalSiteData;
+
+public class DashBoardController implements Initializable {
+	
+	
+	private List<Festival> festivals;
+	private List<HistoricEvent> historicEvents;
+	private List<HistoricalFigure> historicalFigures;
+	private List<HistoricalPeriod> periods;
+	private List<HistoricalSite> historicalSites;
+	private List<King> kings;
 
     @FXML
     private ImageView Exit;
@@ -34,11 +62,26 @@ public class DashBoard implements Initializable {
 
     @FXML
     private StackPane contentArea;
-
+    
+    @FXML
+    private GridPane periodContainer;
+    
     Stage stage = null;
 
+    public DashBoardController(List<Festival> festivals, List<HistoricEvent> historicEvents, List<HistoricalFigure> historicalFigures, List<HistoricalPeriod> periods, List<HistoricalSite> historicalSites, List<King> kings) {
+    	this.festivals = festivals;
+    	this.periods = periods;
+    	this.historicEvents = historicEvents;
+    	this.historicalSites = historicalSites;
+    	this.historicalFigures = historicalFigures;
+    	this.kings = kings;
+    }
+    
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+    	
+    	createPeriodBar();
+         
         Exit.setOnMouseClicked(e -> {
 
             System.exit(0);
@@ -56,10 +99,10 @@ public class DashBoard implements Initializable {
                 stage.setMaximized(true);
         });
 
-
+        
 
         try {
-            Parent fxml = FXMLLoader.load(getClass().getResource("/view/homePage/homePage.fxml"));
+            Parent fxml = FXMLLoader.load(getClass().getResource("/screen/homePage/homePage.fxml"));
             contentArea.getChildren().removeAll();
             contentArea.getChildren().setAll(fxml);
 
@@ -68,9 +111,47 @@ public class DashBoard implements Initializable {
         }
     }
 
+    private void createPeriodBar () {
+    	
+    	HistoricalPeriodData historicalPeriodData = new HistoricalPeriodData("src\\data\\period.json");
+		
+    	ObservableList<HistoricalPeriod> historicalPeriods = FXCollections.observableArrayList(historicalPeriodData.readData());
+
+    	int row = 0;
+    	int column = 2;
+    	
+    	try {
+    		
+	    	for (HistoricalPeriod period : historicalPeriods) {
+	    		
+	    		FXMLLoader fxmlLoader = new FXMLLoader();
+	    		
+	    		fxmlLoader.setLocation(getClass().getResource("/screen/historicalPeriod/historicalPeriod.fxml"));
+	    		
+	    		VBox periodBox = fxmlLoader.load();
+	    		
+	    		HistoricalPeriodController HistoricalPeriodController = fxmlLoader.getController();
+	    		
+	    		if (column == 3) {
+	    			
+	    			column = 0;
+	    			++row;
+	    		}
+	    		
+	    		HistoricalPeriodController.setData(period);
+	    		
+	    		periodContainer.add(periodBox, column ++, row);
+	    		periodContainer.setMargin(periodBox, new Insets(2));
+	    	}
+    	} catch (IOException e) {
+    	
+    	}
+    }
+    
     @FXML
     private void homePage(javafx.event.ActionEvent event) throws IOException {
-        Parent fxml = FXMLLoader.load(getClass().getResource("/view/homePage/homePage.fxml"));
+        Parent fxml = FXMLLoader.load(getClass().getResource("/screen/homePage/homePage.fxml"));
+        
         contentArea.getChildren().removeAll();
         contentArea.getChildren().setAll(fxml);
 
@@ -78,7 +159,11 @@ public class DashBoard implements Initializable {
     
     @FXML
     private void festivals(javafx.event.ActionEvent event) throws IOException {
-        Parent fxml = FXMLLoader.load(getClass().getResource("/view/festival/festival.fxml"));
+    	
+    	FXMLLoader loader = new FXMLLoader(getClass().getResource("/screen/festival/festivalList.fxml"));    	
+    	FestivalsController festivalsController = new FestivalsController(this.festivals);
+    	loader.setController(festivalsController);
+        Parent fxml = loader.load();
         contentArea.getChildren().removeAll();
         contentArea.getChildren().setAll(fxml);
 
@@ -86,36 +171,55 @@ public class DashBoard implements Initializable {
 
     @FXML
     private void historicEvents(javafx.event.ActionEvent event) throws IOException {
-        Parent fxml = FXMLLoader.load(getClass().getResource("/view/historicEvent/historicEvents.fxml"));
+    	
+      	FXMLLoader loader = new FXMLLoader(getClass().getResource("/screen/historicEvent/historicEventList.fxml"));    	
+    	HistoricEventsController historicEventController = new HistoricEventsController(this.historicEvents);
+    	loader.setController(historicEventController);
+        Parent fxml = loader.load();
         contentArea.getChildren().removeAll();
         contentArea.getChildren().setAll(fxml);
     }
 
     @FXML
     private void historicalFigures(javafx.event.ActionEvent event) throws IOException {
-        Parent fxml = FXMLLoader.load(getClass().getResource("/view/historicalFigure/historicalFigures.fxml"));
+    	
+    	FXMLLoader loader = new FXMLLoader(getClass().getResource("/screen/historicalFigure/historicalFigures.fxml"));    	
+    	HistoricalFigureController historicalFiguresController = new HistoricalFigureController(this.historicalFigures);
+    	loader.setController(historicalFiguresController);
+        Parent fxml = loader.load();
         contentArea.getChildren().removeAll();
         contentArea.getChildren().setAll(fxml);
     }
 
     @FXML
     private void historicalSites(javafx.event.ActionEvent event) throws IOException {
-        Parent fxml = FXMLLoader.load(getClass().getResource("/view/historicalSite/historicalSite.fxml"));
+    	
+    	FXMLLoader loader = new FXMLLoader(getClass().getResource("/screen/historicalSite/historicalSites.fxml"));    	
+    	HistoricalSitesController historicalSitesController = new HistoricalSitesController(this.historicalSites);
+    	loader.setController(historicalSitesController);
+        Parent fxml = loader.load();
         contentArea.getChildren().removeAll();
         contentArea.getChildren().setAll(fxml);
     }
 
     @FXML
     private void periods(javafx.event.ActionEvent event) throws IOException {
-        Parent fxml = FXMLLoader.load(getClass().getResource("/view/period/period.fxml"));
+  
+    	FXMLLoader loader = new FXMLLoader(getClass().getResource("/screen/historicalPeriod/historicalPeriodList.fxml"));    	
+    	HistoricalPeriodsController historicalPeriodsController = new HistoricalPeriodsController(this.periods);
+    	loader.setController(historicalPeriodsController);
+        Parent fxml = loader.load();
         contentArea.getChildren().removeAll();
         contentArea.getChildren().setAll(fxml);
-
     }
     
     @FXML
     private void kings(javafx.event.ActionEvent event) throws IOException {
-        Parent fxml = FXMLLoader.load(getClass().getResource("/view/king/king.fxml"));
+    	
+    	FXMLLoader loader = new FXMLLoader(getClass().getResource("/screen/king/king.fxml"));    	
+    	KingController kingController = new KingController(this.kings);
+    	loader.setController(kingController);
+        Parent fxml = loader.load();
         contentArea.getChildren().removeAll();
         contentArea.getChildren().setAll(fxml);
 
--- src/controller/festival/FestivalController.java
+++ src/controller/festival/FestivalController.java
@@ -1,10 +1,44 @@
 package controller.festival;
 
+import java.util.Calendar;
+import java.util.TimeZone;
+
+import com.jfoenix.controls.JFXButton;
+
+import javafx.fxml.FXML;
 import models.festival.Festival;
 
 public class FestivalController {
+
+    @FXML
+    private JFXButton name;
+
+    @FXML
+    private JFXButton time;
+
+    @FXML
+    private JFXButton place;
+
 	
-	private Festival model;
-	private 
-	
+	public void setData(Festival festival) {
+		
+		name.setText(festival.getName());
+		
+		if (festival.getDate() != null) {
+			
+			Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("Vietnam"));
+			
+			cal.setTime(festival.getDate());
+			
+			int month = cal.get(Calendar.MONTH) + 1;
+			int day = cal.get(Calendar.DAY_OF_MONTH) + 1;
+			
+			String heldDate = Integer.toString(day) + "/" + Integer.toString(month); 
+			time.setText(heldDate);
+		} else {
+			time.setText("");
+		}
+
+		place.setText(festival.getPlace());
+	}
 }
